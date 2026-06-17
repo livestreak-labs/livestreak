43d0409b164c50e322aea77965cdbd3ba517f5ec
kelvinpraises <kelvinpraises@gmail.com>
2026-06-17 12:53:10 +0100

    feat(bookmaker): public package exports

--- packages/bookmaker/src/index.ts
+++ packages/bookmaker/src/index.ts
@@ -1,10 +1,31 @@
 export type {
-  BookmakerContractCall,
+  BookmakerDetectionEvaluation,
+  BookmakerDetectionInput,
+  BookmakerDetectionPolicy,
+  PatternDetectionInput,
+  PatternDetector
+} from "./detection/index.js";
+export type { BuildVaultDraftOptions } from "./draft/index.js";
+export type { BookmakerPanelSnapshot } from "./panel/index.js";
+export type { BookmakerSimilarityClient } from "./similarity/index.js";
+export type { BookmakerDuplicatePolicy, BookmakerVaultPolicy } from "./decision/index.js";
+export type { BookmakerRuntimeConfig } from "./runtime/index.js";
+export type {
+  ObservationEvent,
+  ObservationFeed,
+  ObservationSnapshot,
+  ObservationSubscriptionInput
+} from "./observation/index.js";
+export type {
   BookmakerDecision,
   BookmakerMarketContext,
   BookmakerPanelView,
   BookmakerSkipReason,
+  BookmakerWatchRef,
+  BookmakerWatchRefKind,
   BookmakerWatchSource,
+  BookmakerContractsSurface,
+  BookmakerWriteIntent,
   BookmakerWritePlan,
   Detection,
   SimilarityCandidate,
@@ -13,3 +34,54 @@ export type {
   VaultDraft,
   VaultResolutionWindow
 } from "./model/index.js";
+export type {
+  BookmakerExecutableWriteIntent,
+  BookmakerIntentOnlyWriteIntent,
+  BookmakerContractMarketRef
+} from "./write/index.js";
+export type {
+  HostSimilarityDuplicateRisk,
+  HostSimilarityRequest,
+  HostSimilarityResult,
+  HostSimilaritySuggestedAction,
+  HostSimilarityVaultDraft,
+  HostSimilarVaultCandidate
+} from "./similarity/index.js";
+export type {
+  ValidationFailure,
+  ValidationResult,
+  ValidationSuccess
+} from "./validate/index.js";
+export { detectOpportunity } from "./detection/index.js";
+export { buildVaultDraft } from "./draft/index.js";
+export { chooseVaultAction } from "./decision/index.js";
+export {
+  contractsWriteSurfaceAvailable,
+  hasContractsWriteSurface,
+  mapCreateVaultIntentToDescriptor,
+  mapExecutableIntentsToDescriptors,
+  partitionWriteIntents,
+  planBookmakerWrite
+} from "./write/index.js";
+export { projectBookmakerPanel } from "./panel/index.js";
+export {
+  buildObservationSubscriptionInput,
+  validateObservationEvent
+} from "./observation/index.js";
+export {
+  findSimilar,
+  hostSimilarityResultToBookmaker,
+  similarityQueryToHostRequest,
+  vaultDraftToHostSimilarityDraft
+} from "./similarity/index.js";
+export {
+  validateBookmakerDecision,
+  validateBookmakerMarketContext,
+  validateBookmakerRuntimeConfig,
+  validateBookmakerWatchSource,
+  validateDetection,
+  validateSimilarityResult,
+  validateVaultDraft,
+  validationFailure,
+  validationSuccess
+} from "./validate/index.js";
